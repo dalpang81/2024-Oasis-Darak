78c2e91ec00acf38455040ec26e301470cdb2369
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-08-24 00:34:35 +0900

    feat: 기본설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Route>
+        <Route path='/' element={<Layout />}>
+          {/* <Route index element={<LoginPage />} />
+          <Route path='main' element={<MainPage />} />
+          <Route path=':moviesId' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} /> */}
+        </Route>
+      </Route>
     </div>
   );
 }
